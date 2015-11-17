49a4029fcfe983b8f50f53c8782b60c81c3cbc06
Diguilio Martin <martindiguilio@hotmail.com>
2015-11-17 02:19:44 -0300

    Implementado servicio de actualizar los datos del usuario

--- HayCancha/app/src/main/java/sempait/haycancha/fragment/PerfilFragment.java
+++ HayCancha/app/src/main/java/sempait/haycancha/fragment/PerfilFragment.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +16,13 @@ import android.widget.TextView;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 import sempait.haycancha.ConfigurationClass;
+import sempait.haycancha.ConfirmDialogCustom;
 import sempait.haycancha.R;
 import sempait.haycancha.Utils;
 import sempait.haycancha.activities.LoginActivity;
+import sempait.haycancha.base.BaseActivity;
 import sempait.haycancha.base.BaseFragment;
+import sempait.haycancha.services.CreateAccountTask;
 
 /**
  * Created by martin on 16/11/15.
@@ -28,6 +32,8 @@ public class PerfilFragment extends BaseFragment {
     private EditText mTxtUserName, mTxtUserEmail, mTxtUserPassword, mTxtUserPhone, mEtxtLastName;
     private TextView mTxtPosicion;
     private ImageView mImgProfile, mImgArquero, mImgDefensor, mImgDelantero, mImgTodaElCampo;
+    private int mPositionSelectec;
+    private CreateAccountTask mUpdateAccountTask;
 
 
     @Nullable
@@ -66,7 +72,7 @@ public class PerfilFragment extends BaseFragment {
         mTxtUserPhone.setText(ConfigurationClass.getTelUser(mContext));
         String url = "http://3.bp.blogspot.com/-QgXJDaHt5Lo/UpxQPX_W-gI/AAAAAAAANII/TzU2P5KfGxU/s1600/3874_3_1219.jpg";
         ImageLoader.getInstance().displayImage(url.contains("http:") ? url : "http:" + url, mImgProfile, Utils.getImageLoaderOptionRouded());
-
+        refreshPostion(ConfigurationClass.getCodigoPosicion(mContext));
 
     }
 
@@ -117,6 +123,65 @@ public class PerfilFragment extends BaseFragment {
                 refreshPostion(4);
             }
         });
+
+
+        view.findViewById(R.id.btn_update).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (allFildCompleted())
+                    executeUpdateData();
+                else {
+                    ConfirmDialogCustom dialog = new ConfirmDialogCustom(mContext.getString(R.string.error_form_incomplete_message), mContext.getString(R.string.perfil), mContext.getString(R.string.acept_text));
+                    FragmentTransaction ft = ((BaseActivity) mContext).getSupportFragmentManager().beginTransaction();
+                    ft.add(dialog, null);
+                    ft.commitAllowingStateLoss();
+                }
+
+            }
+        });
+    }
+
+    private void executeUpdateData() {
+
+        mUpdateAccountTask = new CreateAccountTask(mContext) {
+
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+
+
+                ConfirmDialogCustom dialog;
+
+
+                if (result != null && result.equalsIgnoreCase("ok")) {
+
+                    dialog = new ConfirmDialogCustom(mContext.getString(R.string.success_update_user_info), mContext.getString(R.string.perfil), mContext.getString(R.string.acept_text));
+
+                } else
+                    dialog = new ConfirmDialogCustom(mContext.getString(R.string.error_message), mContext.getString(R.string.perfil), mContext.getString(R.string.acept_text));
+
+
+                FragmentTransaction ft = ((BaseActivity) mContext).getSupportFragmentManager().beginTransaction();
+                ft.add(dialog, null);
+                ft.commitAllowingStateLoss();
+
+
+            }
+        };
+
+        mUpdateAccountTask.mPosicion = mPositionSelectec;
+        mUpdateAccountTask.mApellido = mEtxtLastName.getText().toString();
+        mUpdateAccountTask.mNombre = mTxtUserName.getText().toString();
+        mUpdateAccountTask.mEmail = mTxtUserEmail.getText().toString();
+        mUpdateAccountTask.mPassword = mTxtUserPassword.getText().toString();
+        mUpdateAccountTask.mTelefono = mTxtUserPhone.getText().toString();
+        mUpdateAccountTask.mCodigoUsuario = ConfigurationClass.getUserCod(mContext);
+
+        mUpdateAccountTask.execute();
+
+
     }
 
     private void refreshPostion(int position) {
@@ -130,6 +195,7 @@ public class PerfilFragment extends BaseFragment {
                 mImgDelantero.setImageResource(R.drawable.delantero_desactivado);
                 mImgTodaElCampo.setImageResource(R.drawable.todo_campo_desactivado);
                 mTxtPosicion.setText("Arquero");
+                mPositionSelectec = 1;
                 break;
 
             case 2:
@@ -138,6 +204,7 @@ public class PerfilFragment extends BaseFragment {
                 mImgDelantero.setImageResource(R.drawable.delantero_desactivado);
                 mImgTodaElCampo.setImageResource(R.drawable.todo_campo_desactivado);
                 mTxtPosicion.setText("Defensor");
+                mPositionSelectec = 2;
                 break;
 
             case 3:
@@ -146,6 +213,7 @@ public class PerfilFragment extends BaseFragment {
                 mImgDelantero.setImageResource(R.drawable.delantero_activo);
                 mImgTodaElCampo.setImageResource(R.drawable.todo_campo_desactivado);
                 mTxtPosicion.setText("Delantero");
+                mPositionSelectec = 3;
                 break;
             case 4:
                 mImgArquero.setImageResource(R.drawable.arquero_desactivado);
@@ -153,9 +221,23 @@ public class PerfilFragment extends BaseFragment {
                 mImgDelantero.setImageResource(R.drawable.delantero_desactivado);
                 mImgTodaElCampo.setImageResource(R.drawable.todo_campo_activo);
                 mTxtPosicion.setText("Todo el campo");
+                mPositionSelectec = 4;
                 break;
 
         }
+
+
+    }
+
+    private boolean allFildCompleted() {
+
+        if (mEtxtLastName.getText().toString().isEmpty()
+                || mTxtUserName.getText().toString().isEmpty() || mTxtUserPhone.getText().toString().isEmpty() || mTxtUserPassword.getText().toString().isEmpty() || mTxtUserEmail.getText().toString().isEmpty()) {
+
+            return false;
+
+        } else
+            return true;
 
 
     }
